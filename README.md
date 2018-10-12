b7910db56194bd371c9d071dd1e1a588ae28e36a
Thomas Schlage <fragsalat@users.noreply.github.com>
2018-10-12 14:13:56 +0200

    Calling lifecycle, projecting children, binding defined props, improving readme (#6)

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # aurelia-react-loader
-Load React components directly from Aurelia views
+This plugin allows you to load and render React/Preact components in your Aurelia application.
 
 ## Installation
-First install the loader plugin with jspm.
+First install the loader plugin.
 
 ```
-jspm install npm:aurelia-react-loader
+au install aurelia-react-loader
 ```
 
 Then register the plugin with Aurelia.
@@ -21,31 +21,108 @@ export function configure(aurelia) {
 }
 ```
 
-## Use
+## Usage
 
-Import react components into Aurelia views just like you import a custom element. Just specify the `!react-component` loader after the module name.
+Import react components into Aurelia views just like you import a custom element. 
+Just specify the `react-component!` loader before the module name. 
 
 In `aurelia-view.html`:
 ```html
 <template>
-  <require from="my-react-component.js!react-component"></require>
-
-  <input ref="in" />
-  <my-react-component props.bind="{ name: in.value, onClick: submit }"></my-react-component>
+  <require from="react-component!my-react-component.js"></require>
+  <my-react-component name.bind="someCrazyName" on-click.bind="submit"></my-react-component>
 </template>
 ```
 
 In `my-react-component.js`:
 ```jsx
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MyReactComponent extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    onClick: PropTypes.func
+  }
+
   render() {
     let { name, onClick } = this.props;
     return (<button onClick={onClick}>Hello, {name}</button>);
   }
 }
 ```
+
+# Use Preact instead of React
+As Preact is way smaller than React I consider it to be the better library to choose (if possible) when adding 3rd-Party components.
+
+#### Install preact
+```bash
+au install preact
+au install preact-compat
+```
+
+#### Mapping React to Preact
+First of all we have to tell our loader to map the load of react to load preact instead. For RequireJS you just have to
+add a small part to the loader section in your aurelia.json. For webpack or SystemJS I don't know. 
+Feel free to tell me how it works so that I can add it here.
+```json
+"loader": {
+  "type": "require",
+  "configTarget": "vendor-bundle.js",
+  "includeBundleMetadataInConfig": "auto",
+  "config": {
+    "map": {
+      "*": {
+        "react": "preact",
+        "react-dom": "preact-compat"
+      }
+    }
+  },
+  ...
+}
+```
+
+With the new aurelia-cli (> 1.0.0-beta.1) this is not enough. To tell the tracer the same like RequireJS we have to rewrite
+the loaded module by using the `onRequiringModule` hook. 
+```js
+function writeBundles() {
+  return buildCLI.dest({
+    onRequiringModule(moduleId) {
+      if (moduleId === 'react') {
+        return ['preact'];
+      }
+      if (moduleId === 'react-dom') {
+        return ['preact-compat'];
+      }
+    }
+  });
+}
+``` 
+
+# How it works
+### General
+The aurelia-react-loader hooks into the require loading process and receives an object of react component classes like `{MyReactComponent: class MyReactComponent}`.
+All keys within this object are the exported classes, vars, etc and the plugin uses them to register the html tag by converting the name to kebab case.
+Now the plugin creates a wrapper class which is actually a aurelia custom element which renders into it's element the react component.
+To be able to bind the component props via aurelia's binding engine the plugin creates a bindable attribute for each property defined in `propTypes`.
+If you haven't defined propTypes you can also bind a object to props. The plugin also considers the defaultProps and passes a merged object to the createElement function.
+
+The react element is rendered into a div container beside possible `<au-content>` first. This ensures all styles are applied
+to your component and the lifecycle componentDidMount is really meaning that it's available in the dom. After the element is
+rendered and the children were projected the rendered component is appended to the custom element's div element and the previous container is removed.
+
+### Content projection
+To enable the content projection into react components the plugin passes `createElement('slot')` as children to the component.
+Once your component used {this.props.children} and a `<slot>` element will be rendered as html the plugin know it must project the content.
+To do that the aurelia-react-loader takes the already rendered children from `<au-content>` and appends them before the `<slot>` inside the component html.
+After the children are moved, the slot placeholder gets removed and the html is appended to the custom element's `<div>` element.
+
+### Lifecycle
+- **CustomElement.attached()** -> React component will be rendered if not done yet and mounted to the dom and therefore `componentDidMount` will be called
+- **CustomElement.detached()** -> React component is unmounted from dom and `componentWillUnmount` will be called 
+- **CustomElement.unbind()** -> React component will be completely destroyed and un-rendered
+- **CustomElement.anyChanged()** -> Once any bound attribute changes `componentWillReceiveProps` will be called and props on component will be updated
+
 
 A few things to note:
 * React component names are converted to kebab case for safe use in HTML. `<MyReactComponent foo={bar} />` in jsx becomes `<my-react-component props.bind="{foo:bar}"></my-react-component>` in an HTML Aurelia template.
--- src/index.js
+++ src/index.js
@@ -1,76 +1,196 @@
-import { noView, customElement, bindable } from 'aurelia-templating';
-import { decorators } from 'aurelia-metadata';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import hyphenate from 'lodash.kebabcase';
+import {noView, customElement, bindable} from 'aurelia-templating';
+import {decorators} from 'aurelia-metadata';
+import {createElement} from 'react';
+import {render} from 'react-dom';
 
-export function configure({ aurelia }) {
-	const loader = aurelia.loader;
-	loader.addPlugin('react-component', {
-		fetch(address) {
-			return loader.loadModule(address)
-				.then(getReactCustomElements);
-		}
-	});
+/**
+ * Configure the aurelia loader to use handle urls with !component
+ * @param {FrameworkConfiguration} config
+ */
+export function configure(config) {
+  const loader = config.aurelia.loader;
+  loader.addPlugin('react-component', {
+    fetch(address) {
+      return loader.loadModule(address)
+        .then(getComponents);
+    }
+  });
 }
 
-function getReactCustomElements(module) {
-	const elements = {};
-
-	for (const name in module) {
-		if (module.hasOwnProperty(name)) {
-			const component = module[name];
-			if (typeof component === 'function') {
-				const elementName = hyphenate(name);
-				elements[elementName] = createReactElement(component, elementName);
-			}
-		}
-	}
-
-	return elements;
+/**
+ * Extract the components from the loaded module
+ * @param {Object} module Object containing all exported properties
+ * @returns {Object}
+ */
+export function getComponents(module) {
+  return Object.keys(module).reduce((elements, name) => {
+    if (typeof module[name] === 'function') {
+      const elementName = camelToKebab(name);
+      elements[elementName] = wrapComponent(module[name], elementName);
+    }
+    return elements;
+  }, {});
 }
 
-function createReactElement(component, name) {
-	return decorators(
-		noView(),
-		customElement(name),
-		bindable({
-			name: 'props',
-			attribute: 'props',
-			changeHandler: 'propsChanged',
-			defaultBindingMode: 1,
-		})).on(createCustomElementClass(component));
+/**
+ * Converts camel case to kebab case
+ * @param {string} str
+ * @returns {string}
+ */
+function camelToKebab(str) {
+  // Matches all places where a two upper case chars followed by a lower case char are and split them with an hyphen
+  return str.replace(/([a-zA-Z])([A-Z][a-z])/g, (match, before, after) =>
+    `${before.toLowerCase()}-${after.toLowerCase()}`
+  ).toLowerCase();
 }
 
-function createCustomElementClass(component) {
-	return class ReactComponent {
-		static inject() {
-			return [ Element ];
-		}
+/**
+ * Wrap the React components into an ViewModel with bound attributes for the defined PropTypes
+ * @param {Object} component
+ * @param {string} elementName
+ * @returns {Object}
+ */
+function wrapComponent(component, elementName) {
+  let bindableProps = [];
+  if (component.propTypes) {
+    bindableProps = Object.keys(component.propTypes).map(prop => bindable({
+      name: prop,
+      attribute: camelToKebab(prop),
+      changeHandler: 'updateProps',
+      defaultBindingMode: 1
+    }));
+  }
+  return decorators(
+    noView(),
+    customElement(elementName),
+    bindable({name: 'props', attribute: 'props', changeHandler: 'updateProps', defaultBindingMode: 1}),
+    ...bindableProps
+  ).on(createWrapperClass(component));
+}
 
-		constructor(element) {
-			this.element = element;
-			this.component = null;
-		}
+/**
+ * Create a wrapper class for the component
+ * @param {Object} component
+ * @returns {WrapperClass}
+ */
+function createWrapperClass(component) {
+  return class WrapperClass {
+    static inject = [Element];
 
-		propsChanged() {
-			this.render();
-		}
+    /**
+     * @param {Element} element
+     */
+    constructor(element) {
+      this.element = element;
+    }
 
-		bind() {
-			this.render();
-		}
+    /**
+     * Re-render the Preact component when values changed
+     */
+    attached() {
+      if (!this.component) {
+        this.render();
+      } else if (typeof this.component.componentDidMount === 'function') {
+        this.component.componentDidMount();
+      }
+    }
 
-		unbind() {
-			ReactDOM.unmountComponentAtNode(this.element);
-			this.component = null;
-		}
+    /**
+     * Triggers un-mound function to release events
+     */
+    detached() {
+      if (this.component && typeof this.component.componentWillUnmount === 'function') {
+        this.component.componentWillUnmount();
+      }
+    }
 
-		render() {
-			this.component = ReactDOM.render(
-				React.createElement(component, this.props),
-				this.element
-			);
-		}
-	};
+    /**
+     * Un-render the component
+     */
+    unbind() {
+      this.component = null;
+      this.element.component = null;
+      render('', this.element, this.component);
+    }
+
+    /**
+     * Determine props passed to create react elements
+     * @returns {Object}
+     */
+    getProps() {
+      const props = this.props || {};
+      // Copy bound properties because Object.assign doesn't work deep
+      for (const prop in this) {
+        if (this[prop] !== undefined && typeof this[prop] !== 'function') {
+          props[prop] = this[prop] === '' ? true : this[prop];
+        }
+      }
+      delete props.element;
+
+      return Object.assign({}, component.defaultProps, props);
+    }
+
+    /**
+     * Will be called when bindable updated
+     */
+    updateProps() {
+      if (this.component && typeof this.component.componentWillReceiveProps === 'function') {
+        const props = this.getProps();
+        this.component.componentWillReceiveProps(props);
+        this.component.props = props;
+      }
+    }
+
+    /**
+     * Render Preact component
+     */
+    render() {
+      // Create container in active dom to apply styles already
+      const container = document.createElement('div');
+      this.element.appendChild(container);
+
+      // Render react component with a slot as children into a container to possibly replace the slot with real children
+      const reactElement = createElement(component, this.getProps(), createElement('slot'));
+      this.component = render(reactElement, container);
+      this.element.component = this.component;
+
+      const slot = container.querySelector('slot');
+      // If no slot is rendered the component doesn't accept children
+      if (slot) {
+        const content = this.element.querySelector('au-content');
+        if (!content) {
+          return;
+        }
+        // Move original children to slot position
+        for (let i = 0; i < content.children.length; i++) {
+          slot.parentNode.insertBefore(content.children[i], slot);
+        }
+        slot.parentNode.removeChild(slot);
+        this.insertContainerContent(container, content);
+      } else {
+        this.insertContainerContent(container);
+      }
+    }
+
+    /**
+     * Moves content of the container into the correct place within this element
+     * @param {HTMLElement} container
+     * @param {HTMLElement} replacement
+     */
+    insertContainerContent(container, replacement) {
+      // Append child to fragment to get rid of container element which can break element flow
+      const fragment = document.createDocumentFragment();
+      for (let i = 0; i < container.children.length; i++) {
+        fragment.appendChild(container.children[i]);
+      }
+      // Either replace au-content or just append if no children are passed
+      if (replacement) {
+        this.element.replaceChild(fragment, replacement);
+      } else {
+        this.element.appendChild(fragment);
+      }
+      // Container is now obsolete as the children are laying directly under the parent
+      this.element.removeChild(container);
+    }
+  };
 }
